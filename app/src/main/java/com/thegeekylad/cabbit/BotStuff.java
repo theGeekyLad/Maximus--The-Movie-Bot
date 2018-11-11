8ea2fb0ee5f6222a182cc1cd81b43e25fd72c744
Rahul Pillai <rahul.pillai03@gmail.com>
2018-11-11 23:15:02 +0530

    removed api keys

--- app/src/main/java/com/thegeekylad/cabbit/BotStuff.java
+++ app/src/main/java/com/thegeekylad/cabbit/BotStuff.java
@@ -21,12 +21,10 @@ import java.util.List;
 
 public class BotStuff {
 
-    // private static String WORKSPACE_ID = "3f77d8c5-d09e-4c3f-85ac-94bb8d5e2bdc"; // cabbit
-    // private static String WORKSPACE_ID = "62341fe6-7cd8-4ba1-a0d9-bf21a1b1e1dc"; // network 360
-    private static String WORKSPACE_ID = "b6bbabe2-c8fb-4973-98f1-5e290f53b44b"; // maximus
-    private static String USERNAME = "b63520ec-d4d4-45fa-a25e-288509c04307";
-    private static String PASSWORD = "bwzG4gTYOLX8";
-    private static String OMDB_API_KEY = "7f7a0e3f";
+    private static String WORKSPACE_ID = "";
+    private static String USERNAME = "";
+    private static String PASSWORD = "";
+    private static String OMDB_API_KEY = "";
 
     private Assistant assistant;
     private android.content.Context context;
@@ -36,8 +34,6 @@ public class BotStuff {
     public BotStuff(android.content.Context context) {
         this.context = context;
         assistant = new Assistant("2018-02-16");
-        // assistant.setUsernameAndPassword("dc98b13f-b5ae-479d-927c-e8852d056f06","FUP2l2YQxhQN"); // cabbit
-        // assistant.setUsernameAndPassword("b63520ec-d4d4-45fa-a25e-288509c04307","bwzG4gTYOLX8"); // network 360
         assistant.setUsernameAndPassword(USERNAME,PASSWORD); // maximus
         assistant.setEndPoint("https://gateway.watsonplatform.net/conversation/api");
         botContext = new Context();
